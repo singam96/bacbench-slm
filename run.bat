1515c1b36b5d3bd76cdc88cee4a153ccfdc5e779
Sachin <sachin.george96@gmail.com>
2026-01-10 02:04:57 +0530

    better run

--- run.bat
+++ run.bat
@@ -18,6 +18,11 @@ echo - Run: python train.py
 docker run --rm -it --gpus all ^
   -v "%cd%":/workspace ^
   -v bacbench-trainer-cache:/workspace/.cache ^
+  -e XDG_CACHE_HOME=/workspace/.cache ^
+  -e TORCH_HOME=/workspace/.cache/torch ^
+  -e HF_HOME=/workspace/.cache/huggingface ^
+  -e HF_HUB_CACHE=/workspace/.cache/huggingface/hub ^
+  -e HF_DATASETS_CACHE=/workspace/.cache/huggingface/datasets ^
   -w /workspace ^
   %IMAGE_NAME% bash
 
